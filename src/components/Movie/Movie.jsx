bf829632593f1e93dea861fa6b0a903d2a19db49
Абдулкаримов Олег <279015582@TOP.ACADEMY>
2024-11-28 19:11:54 +0300

    Добавлена картинка заглушка

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -3,8 +3,11 @@ import './Movie.css';
 function Movie({ movie }) {
     let { poster, name, alternativeName, year } = movie;
 
+    let imageUrl = poster.previewUrl || 
+    'https://png.pngtree.com/png-vector/20220626/ourmid/pngtree-cinema-clapper-movie-thing-video-device-clap-black-icon-action-studio-png-image_5472994.png'
+
     return (<div className="movie movie-card">
-        <img className='movie__poster' src={ poster.previewUrl } />
+        <img className='movie__poster' src={ imageUrl } />
         <h3>{ name || alternativeName }</h3>
         <p>{ year }</p>
     </div>)
